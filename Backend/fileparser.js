637169c73c73721c5767662df0effac13f8bed87
karanrajput2003 <97956908+karanrajput2003@users.noreply.github.com>
2024-11-03 20:35:07 +0530

    Update fileparser.js

--- Backend/fileparser.js
+++ Backend/fileparser.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid'); // For generating unique IDs
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const region = process.env.S3_REGION;
-const Bucket = process.env.S3_BUCKET;
+const Bucket = 'aarogyadocuments';
 
 const parsefile = async (req) => {
     return new Promise((resolve, reject) => {
